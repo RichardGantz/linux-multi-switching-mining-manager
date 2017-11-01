3256faf0021177a675a3d8d8325cc39cc750741f
Richard <richard.gantz@online.de>
2017-11-01 21:51:34 +0100

    Tweaking und Benchmarking, Auswahl über Startparameter
    1. bench_30s_2.sh:
        Defaultverhalten: Automatischer Abbruch mit Berechnung und Eintrag von Watt, Hash und NiceHashID in benchmark_UUID.json
                                      nach mindestens 30 Sekunden UND mindestens 20 Hashwerten.
        Mit Startparameter 
                  -h|--help     bekommt man eine Hilfe zum Programmaufruf
                  -w TIME       kann man die Minimum Wattzahlenermittlung in Sekunden setzen.
                  -m HASHES  kann man die Anzahl Hashwerte setzen
                  Nach wie vor wird das Programm automatisch mit den entsprechenden Berechnungen beendet,
                  wenn BEIDE Werte abgearbeitet sind.
    
                  -t                  TWEAK-Mode. Das Programm läuft endlos, bis es über Ctrl-C, oder kill -15 abgebrochen wird.
                                       Das Programm tweak_commands.sh sendet einen solchen kill -15, wenn es beendet wird.
        Einlesen der Algonamen un IDs aus dem Web oder einer entsprechenden .json Datei, die noch von algo_muti_abfrage.sh da ist
        Keine Berechnungen mehr für Algo "scrypt"
        ccminer holt sich seine internen Algonamen selbst aus der Datei algo_zu_ccminer-algo !!!!!!!!!!!!!!!!!!!!
    
    2. tweak_commands.sh
        Eine Datei, die die Tweak-Kommandos entgegennimmt, protokolliert und ausführt
        und die beim Beenden auch das bench_30s_2.sh beendet, welches seinerseits den CCminer beendet und aufräumt.

---
+++ benchmarking/tweak_commands.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+###############################################################################
+#
+# Entgegennehmen, absetzen und protokollieren von TWEAK-Kommandos
+#
+#
+
+function _On_Exit () {
+    # CCminer stoppen
+    if [ -s bench_30s_2.pid ]; then kill $(cat "bench_30s_2.pid"); fi
+}
+trap _On_Exit EXIT
+
+WISHED_LOGFILES=$(< tweak_to_these_logs)
+read OWN_LOGFILE WATT_LOGFILE HASH_LOGFILE <<<${WISHED_LOGFILES}
+
+echo "Benchmark is actually running for the following GPU:"
+echo "GPU #$(cat bensh_gpu_30s_.index) with UUID: $(cat "uuid")"
+
+echo "Die gewünschte Logdatei für die Tweaking-Kommandos lautet: $OWN_LOGFILE"
+echo "Zusätzlich muss das Kommando in die folgenden Dateien geschrieben werden:"
+echo "Wattwerte: $WATT_LOGFILE und Hashwerte: $HASH_LOGFILE"
+
+while :; do
+
+    read -p "Welches Kommando für diese GPU? " cmd
+
+    case "$cmd" in
+        quit|break|ende|stop)
+            exit 0
+            ;;
+    esac
+    
+    echo "${cmd}" | tee -a ${WATT_LOGFILE} ${HASH_LOGFILE} >>${OWN_LOGFILE}
+    ${cmd}
+done
